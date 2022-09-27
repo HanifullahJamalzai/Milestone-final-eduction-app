b7f998a4f483e06d6ff06d41287812c369ce3852
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-09-27 18:05:33 +0430

    migration and  models created

---
+++ database/migrations/2022_09_27_132117_create_trainers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('bio');
+            $table->string('category');
+            $table->string('photo');
+            $table->string('fb_link');
+            $table->string('twitter_link');
+            $table->string('instagram_link');
+            $table->string('linkedin_link');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainers');
+    }
+};
