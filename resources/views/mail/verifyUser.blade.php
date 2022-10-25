703bddb5974ae97c0f14d4900ad8eaa0fc4e45f3
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-10-25 18:28:23 +0430

    VerifyUser Mail and blade file created

---
+++ resources/views/mail/verifyUser.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Verify Mail</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni odio totam aperiam necessitatibus earum! Quia quibusdam mollitia perferendis tempora. Eveniet animi a et dolores? Consectetur voluptates tempore voluptas dolor exercitationem?</p>
+    <br>
+    <br>
+    <a href="http://127.0.0.1:8000/verify/{{ $token }}">Click to verify Email</a>
+</body>
+</html>
